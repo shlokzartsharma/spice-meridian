ea8be54b638114bc7c1b23f09ae3e6d72f15e62e
Shlok Sharma <shlokzartsharma@gmail.com>
2025-08-10 23:40:30 -0400

    took out feedback widget

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: {
+    buildActivity: false,
+    buildActivityPosition: 'bottom-right',
+  },
+  env: {
+    NEXT_TELEMETRY_DISABLED: '1',
+    NEXT_FEEDBACK_DISABLED: '1',
+  },
 };
 
 export default nextConfig;
